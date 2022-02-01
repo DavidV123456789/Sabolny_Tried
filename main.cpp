a8233a7bc58e318a42cfb22477c49506c52f3eed
map <1david.vdoviak>
2022-02-01 13:15:31 +0100

    j

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 #include "sablonyTried.h"
 int main() {
     //std::cout << "Hello, World!" << std::endl;
-    Obdlznik<int> ABCD(3,5);
-    Obdlznik<float> EFGH(3.6);
-    Obdlznik<double> WXYZ(5.647885);
+    Obdlznik<int,int,15> ABCD(3,5);
+    Obdlznik<float,float> EFGH(3.6);//vyuzije implicitnu hodnotu pre treti paramter
+    Obdlznik<double> WXYZ(5.647885);//vyuzije implicitnu hodnotu pre jeden typ aj treti parameter
 
     std::cout<<ABCD.getObvod()<<std::endl;
     std::cout<<EFGH.getObsah()<<std::endl;
--- sablonyTried.h
+++ sablonyTried.h
@@ -5,33 +5,33 @@
 #ifndef SABOLNY_TRIED_SABLONYTRIED_H
 #define SABOLNY_TRIED_SABLONYTRIED_H
 
-template<typename Any, typename Other=int>//dávame najavo že budeme vytvárať generickú triedu
+template<typename Any, typename Other=int, int pocet=10>//dávame najavo že budeme vytvárať generickú triedu
 class Obdlznik{
 private:
     Any a;
     Any b;
 public:
     Obdlznik():a(1),b(1){};
-    Obdlznik(Any x):a(x),b(x){};
+    explicit Obdlznik(Any x):a(x),b(x){};
     Obdlznik(Any x, Any y):a(x),b(y){};
     Any getObvod() const;
     Any getObsah() const;
-    auto getObvod1()-> decltype(a,b) const;//prakticky alternativny zapis ku: Any getObvod() const;
+    auto getObvod1()-> decltype(a+b) const;//prakticky alternativny zapis ku: Any getObvod() const;//
 };
 
-template<typename Any, typename Other>
-Any Obdlznik<Any,Other>::getObvod() const {
+template<typename Any, typename Other, int pocet>
+Any Obdlznik<Any,Other,pocet>::getObvod() const {
     return 2*a+2*b;
 }
 
-template<typename Any, typename Other>
-Any Obdlznik<Any,Other>::getObsah() const {
+template<typename Any, typename Other, int pocet>
+Any Obdlznik<Any,Other,pocet>::getObsah() const {
     return a*b;
 }
 
-template<typename Any, typename Other>
-auto Obdlznik<Any, Other>::getObvod1() -> decltype(a, b) const {
-    return 2*a+2*b;
+template<typename Any, typename Other, int pocet>
+auto Obdlznik<Any,Other,pocet>::getObvod1() -> decltype(a+b) const {
+    return (2*a+2*b);
 }
 
 #endif //SABOLNY_TRIED_SABLONYTRIED_H
